37f4f9535d482982e32aeda0cf672475e6d9dee9
Zain Andani <710714@pdsb.net>
2021-12-11 15:27:34 -0500

    Added electric boogaloo protocol

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
+java.util.scanner;
 
 public class drivetrain {
+
+    Boolean Drive = false;
+
+    scanner input = new scanner(System.in);
+
+    System.out.println("Robot Go?");
+    input.nextLine();
+
+    if (input == "yis"){
+
+        Drive = true;
+
+    }else{
+
+        Dont drive
+
+    }
+
+    if (input == pineapple){
+
+        init System.this @param (args[System.out.println("Initiate Electric Boogaloo Protocol")]);
+        Initiate boogie woogies;
+
+    }else{
+
+        fix errors
+
+    }
 
 }
