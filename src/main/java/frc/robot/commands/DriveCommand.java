d3f109845d7f602adc8b1ab51dd572c882ead3b0
Zain Andani <710714@pdsb.net>
2021-12-12 13:41:33 -0500

    ye 2

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,10 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.Robot;
 
 public class DriveCommand extends Command{
-
+    private final DriveSubsystem driveSubsystem;
     public DriveCommand() {
 
     }
