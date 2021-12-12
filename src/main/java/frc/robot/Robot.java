d9ffec30f846f2e1eef689408a43bad253571806
Zain Andani <710714@pdsb.net>
2021-12-12 12:24:33 -0500

    init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.DriveCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
   public static DriveSubsystem driveSubsystem = new DriveSubsystem();
 
   private Command m_autonomousCommand;
+  Command driveCommand = new DriveCommand();
 
   private RobotContainer m_robotContainer;
 
